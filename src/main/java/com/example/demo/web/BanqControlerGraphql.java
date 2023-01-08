e83466b09c9d5117a87c5bd87e35bfbe73289587
Alpha oumar Diallo <alpha-oumar.diallo.11@ulaval.ca>
2023-01-08 14:46:05 +0100

    test

--- src/main/java/com/example/demo/web/BanqControlerGraphql.java
+++ src/main/java/com/example/demo/web/BanqControlerGraphql.java
@@ -3,6 +3,8 @@ package com.example.demo.web;
 
 import com.example.demo.entity.BanqueTest;
 import com.example.demo.repositori.BanqueRepositori;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -10,10 +12,15 @@ import java.util.List;
 
 @Controller
 public class BanqControlerGraphql {
-
+@Autowired
     private BanqueRepositori banqueRepositori;
     @QueryMapping
     public List<BanqueTest> compteslistes(){
         return banqueRepositori.findAll();
+    }
+    @QueryMapping
+    public BanqueTest comptelist(@Argument String id){
+        return banqueRepositori.findById(id).
+                orElseThrow(()->new RuntimeException(String.format("acount %s not find", id)));
     }
 }
